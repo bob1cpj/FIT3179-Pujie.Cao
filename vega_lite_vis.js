32bbe10cc6edfc385027c654dffa0b7731a6c273
bob1cpj <161774074+bob1cpj@users.noreply.github.com>
2024-09-24 18:40:22 +1000

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,4 @@
+var vg_1 = "Average_Rainfall_AUS.vg.json";
+vegaEmbed("#map", vg_1).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
