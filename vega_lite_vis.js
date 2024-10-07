56218dc252ffb1f68a0c174bb8597879a2f59b57
bob1cpj <161774074+bob1cpj@users.noreply.github.com>
2024-10-07 11:20:01 +1100

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,3 +2,8 @@ var vg_1 = "Average_Rainfall_AUS.vg.json";
 vegaEmbed("#map", vg_1).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_2 = "MaxTemp_AUS.vg.json";
+vegaEmbed("#scatter", vg_2).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
